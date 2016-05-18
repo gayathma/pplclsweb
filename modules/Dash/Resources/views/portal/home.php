cc12c4c4bb04671b041941ab4432e7880f8710b7
Pathum Senaratna <Pathum Senaratna>
2016-05-18 15:27:12 +0530

    People Clues Basic Template

--- modules/Dash/Resources/views/portal/home.php
+++ modules/Dash/Resources/views/portal/home.php
@@ -1,6 +1,28 @@
 
 <div class="row">
 
+
+
+			<div class="col-lg-3 col-md-6">
+				<div class="card-box widget-user">
+					<div class="text-center">
+						<h2 class="text-custom" data-plugin="counterup">1254</h2>
+						<h5>Total Employees</h5>
+					</div>
+				</div>
+			</div>
+
+
+	<div class="col-lg-3 col-md-6">
+		<div class="card-box widget-user">
+			<div class="text-center">
+				<h2 class="text-warning" data-plugin="counterup">11</h2>
+				<h5>Number of Roles Detected</h5>
+			</div>
+		</div>
+	</div>
+
+
 	<div class="col-lg-3 col-md-6">
 		<div class="card-box">
 			<div class="dropdown pull-right">
@@ -16,23 +38,24 @@
 				</ul>
 			</div>
 
-			<h4 class="header-title m-t-0 m-b-30">Total Revenue</h4>
+			<h4 class="header-title m-t-0 m-b-30">Current Success Ratio</h4>
 
 			<div class="widget-chart-1">
 				<div class="widget-chart-box-1">
-					<input data-plugin="knob" data-width="80" data-height="80" data-fgColor="#f05050 "
-					data-bgColor="#F9B9B9" value="58"
-					data-skin="tron" data-angleOffset="180" data-readOnly=true
-					data-thickness=".15"/>
+					<input data-plugin="knob" data-width="80" data-height="80" data-fgColor="#10c469"
+						   data-bgColor="#AAE2C6" value="20"
+						   data-skin="tron" data-angleOffset="180" data-readOnly=true
+						   data-thickness=".15"/>
 				</div>
-
 				<div class="widget-detail-1">
-					<h2 class="p-t-10 m-b-0"> 256 </h2>
-					<p class="text-muted">Revenue today</p>
+					<p class="text-muted">Based on </p>
+					<h2 class="p-t-10 m-b-0" data-plugin="counterup"> 1020 </h2>
+					<p class="text-muted">Employees </p>
 				</div>
 			</div>
 		</div>
-	</div><!-- end col -->
+
+	</div>
 
 	<div class="col-lg-3 col-md-6">
 		<div class="card-box">
@@ -49,90 +72,28 @@
 				</ul>
 			</div>
 
-			<h4 class="header-title m-t-0 m-b-30">Sales Analytics</h4>
+			<h4 class="header-title m-t-0 m-b-30">Success Ratios</h4>
+			<p class="text-muted">Based on Algorithms</p>
+			<p class="font-600 m-b-5">Linear Regression Algorithm <span class="text-primary pull-right">80%</span></p>
+			<div class="progress progress-bar-primary-alt progress-sm m-b-20">
+				<div class="progress-bar progress-bar-primary progress-animated wow animated animated" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%; visibility: visible; animation-name: animationProgress;">
+				</div><!-- /.progress-bar .progress-bar-danger -->
+			</div><!-- /.progress .no-rounded -->
 
-			<div class="widget-box-2">
-				<div class="widget-detail-2">
-					<span class="badge badge-success pull-left m-t-20">32% <i class="zmdi zmdi-trending-up"></i> </span>
-					<h2 class="m-b-0"> 8451 </h2>
-					<p class="text-muted m-b-25">Revenue today</p>
-				</div>
-				<div class="progress progress-bar-success-alt progress-sm m-b-0">
-					<div class="progress-bar progress-bar-success" role="progressbar"
-					aria-valuenow="77" aria-valuemin="0" aria-valuemax="100"
-					style="width: 77%;">
-					<span class="sr-only">77% Complete</span>
-				</div>
-			</div>
+			<p class="font-600 m-b-5">Fuzzy Algorithm <span class="text-warning pull-right">65%</span></p>
+			<div class="progress progress-bar-warning-alt progress-sm m-b-20">
+				<div class="progress-bar progress-bar-warning progress-animated wow animated animated" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width: 65%; visibility: visible; animation-name: animationProgress;">
+				</div><!-- /.progress-bar .progress-bar-warning -->
+			</div><!-- /.progress .no-rounded -->
+
+			<p class="font-600 m-b-5">Hungarian Algorithm <span class="text-success pull-right">40%</span></p>
+			<div class="progress progress-bar-success-alt progress-sm m-b-0">
+				<div class="progress-bar progress-bar-success progress-animated wow animated animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%; visibility: visible; animation-name: animationProgress;">
+				</div><!-- /.progress-bar .progress-bar-success -->
+			</div><!-- /.progress .no-rounded -->
+
+
 		</div>
 	</div>
-</div><!-- end col -->
-
-<div class="col-lg-3 col-md-6">
-	<div class="card-box">
-		<div class="dropdown pull-right">
-			<a href="#" class="dropdown-toggle card-drop" data-toggle="dropdown" aria-expanded="false">
-				<i class="zmdi zmdi-more-vert"></i>
-			</a>
-			<ul class="dropdown-menu" role="menu">
-				<li><a href="#">Action</a></li>
-				<li><a href="#">Another action</a></li>
-				<li><a href="#">Something else here</a></li>
-				<li class="divider"></li>
-				<li><a href="#">Separated link</a></li>
-			</ul>
-		</div>
-
-		<h4 class="header-title m-t-0 m-b-30">Statistics</h4>
-
-		<div class="widget-chart-1">
-			<div class="widget-chart-box-1">
-				<input data-plugin="knob" data-width="80" data-height="80" data-fgColor="#ffbd4a"
-				data-bgColor="#FFE6BA" value="80"
-				data-skin="tron" data-angleOffset="180" data-readOnly=true
-				data-thickness=".15"/>
-			</div>
-			<div class="widget-detail-1">
-				<h2 class="p-t-10 m-b-0"> 4569 </h2>
-				<p class="text-muted">Revenue today</p>
-			</div>
-		</div>
-	</div>
-</div><!-- end col -->
-
-<div class="col-lg-3 col-md-6">
-	<div class="card-box">
-		<div class="dropdown pull-right">
-			<a href="#" class="dropdown-toggle card-drop" data-toggle="dropdown" aria-expanded="false">
-				<i class="zmdi zmdi-more-vert"></i>
-			</a>
-			<ul class="dropdown-menu" role="menu">
-				<li><a href="#">Action</a></li>
-				<li><a href="#">Another action</a></li>
-				<li><a href="#">Something else here</a></li>
-				<li class="divider"></li>
-				<li><a href="#">Separated link</a></li>
-			</ul>
-		</div>
-
-		<h4 class="header-title m-t-0 m-b-30">Daily Sales</h4>
-
-		<div class="widget-box-2">
-			<div class="widget-detail-2">
-				<span class="badge badge-pink pull-left m-t-20">32% <i class="zmdi zmdi-trending-up"></i> </span>
-				<h2 class="m-b-0"> 158 </h2>
-				<p class="text-muted m-b-25">Revenue today</p>
-			</div>
-			<div class="progress progress-bar-pink-alt progress-sm m-b-0">
-				<div class="progress-bar progress-bar-pink" role="progressbar"
-				aria-valuenow="77" aria-valuemin="0" aria-valuemax="100"
-				style="width: 77%;">
-				<span class="sr-only">77% Complete</span>
-			</div>
-		</div>
-	</div>
-</div>
-</div><!-- end col -->
-
 </div>
                         <!-- end row -->
